9b25952f88915c4e8bb7205412ec1db0bc6cec4d
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-11 21:53:37 -0400

    linted ch 2 examples

--- CSharpExamples/Chapter02/Program.cs
+++ CSharpExamples/Chapter02/Program.cs
@@ -3,7 +3,6 @@
 // TODO: Make the Examples class available from the namespace
 using Chapter02;
 
-
 /* DECLARING AND INITIALIZING VARIABLES */
 
 // TODO: Declare and initialize several variables
@@ -19,9 +18,10 @@ double areaOfCircle = Math.PI * 5 * 5;
 // TODO: Print sentences to the console using the variables above
 Console.WriteLine("We are currently in " + currentUnit);
 Console.WriteLine("Unit 1 is " + (unit1Finished ? "finished" : "unfinished"));
-Console.WriteLine("The " + cohortMonth + " " + cohortYear + " cohort is " + percentComplete1 + "% done.");
+Console.WriteLine(
+    "The " + cohortMonth + " " + cohortYear + " cohort is " + percentComplete1 + "% done."
+);
 Console.WriteLine("The area of a circle of radius 5 is " + areaOfCircle);
-
 
 /* GETTING USER INPUT */
 
@@ -45,9 +45,13 @@ Console.WriteLine("Hello, " + fullName + "!");
 // TODO: Get user input that needs to be converted to other data types
 
 // int
-Console.WriteLine("Welcome to Pizza Surprise. Please enter the number of pizzas you would like to order: ");
+Console.WriteLine(
+    "Welcome to Pizza Surprise. Please enter the number of pizzas you would like to order: "
+);
 int numPizzas = int.Parse(Console.ReadLine());
-Console.WriteLine("Thank you. We will surprise you with " + numPizzas + " pizzas with random toppings.");
+Console.WriteLine(
+    "Thank you. We will surprise you with " + numPizzas + " pizzas with random toppings."
+);
 
 // bool
 Console.WriteLine("True or false: These surprise pizzas are delicious. ");
@@ -55,10 +59,11 @@ bool areDelicious = bool.Parse(Console.ReadLine());
 Console.WriteLine("Your answer: " + (areDelicious ? "YES!" : "...not so much"));
 
 // double
-Console.WriteLine("On a scale of 0.1000000000 (truly terrible) to 10.0000000000 (absolutely amazing), how would you rate the Pizza Surprise concept overall?");
+Console.WriteLine(
+    "On a scale of 0.1000000000 (truly terrible) to 10.0000000000 (absolutely amazing), how would you rate the Pizza Surprise concept overall?"
+);
 double rating = double.Parse(Console.ReadLine());
 Console.WriteLine("Your rating: " + rating);
-
 
 /* STRING METHODS */
 
@@ -73,7 +78,6 @@ Console.WriteLine(bizName.Substring(spaceIdx + 1));
 
 // TODO: Print the result of checking if the word "pizza" is in the name (case-insensitive)
 Console.WriteLine(bizName.ToLower().Contains("pizza"));
-
 
 /* ARRAYS */
 
@@ -97,11 +101,10 @@ Console.WriteLine(toppings); // prints type only
 // TODO: Use the string method .Join() to print the contents of the array
 Console.WriteLine(string.Join(", ", toppings));
 
-
 /* INITIALIZING AN ARRAY WITH VALUES */
 
 // Surprise pizzas!
-string[] pizzas = {"anchovy and pineapple", "sauerkraut and tomato", "corn and carrot"};
+string[] pizzas = { "anchovy and pineapple", "sauerkraut and tomato", "corn and carrot" };
 
 // TODO: Print a sentence with the third type of pizza using bracket notation
 Console.WriteLine("I tried a " + pizzas[2] + " pizza and it wasn't truly terrible.");
@@ -109,11 +112,11 @@ Console.WriteLine("I tried a " + pizzas[2] + " pizza and it wasn't truly terribl
 // TODO: Print the list of pizzas, one on each line, without looping
 Console.WriteLine(string.Join("\n", pizzas));
 
-
 /* WRITING METHODS */
 
 // TODO: Code a method
-static string GetFullName(string firstName, string lastName) {
+static string GetFullName(string firstName, string lastName)
+{
     return firstName + " " + lastName;
 }
 
@@ -122,7 +125,6 @@ static string GetFullName(string firstName, string lastName) {
 string johnSmith = GetFullName("John", "Smith");
 
 Console.WriteLine(johnSmith);
-
 
 /* USING METHODS FROM OTHER CLASSES */
 
