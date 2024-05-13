72e6edc5e3d980984899fa1e2c434872818ee9c2
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-13 00:49:53 -0400

    reset TODOs for ch 3 examples

--- CSharpExamples/Chapter03/Dictionaries.cs
+++ CSharpExamples/Chapter03/Dictionaries.cs
@@ -11,35 +11,25 @@ namespace Chapter03
             // TODO: Declare an empty dictionary, add key/value pairs, and print the map
 
             // Create a dictionary, elfAges, that can hold String keys and Integer values
-            Dictionary<string, int> elfAges = []; // or, new Dictionary<string, int>()
 
             // Add 4 key/value pairs to it representing the elves and their ages
-            elfAges.Add("Legolas", 2931);
-            elfAges.Add("Arwen", 2901);
-            elfAges.Add("Elrond", 6497);
-            elfAges.Add("Galadriel", 8382);
 
             // Print Legolas' age
-            Console.WriteLine("Legolas is " + elfAges["Legolas"] + " years old.");
+
 
             /* DICTIONARY METHODS */
 
             // TODO: Practice using different methods of the Dictionary class
 
             // Print the count
-            Console.WriteLine(elfAges.Count);
 
             // Print just the keys
-            Console.WriteLine(string.Join(", ", elfAges.Keys));
 
             // Print just the values
-            Console.WriteLine(string.Join(", ", elfAges.Values));
 
             // Check if it contains a certain key and print result
-            Console.WriteLine(elfAges.ContainsKey("Arwen"));
 
             // Check if it contains a certain value and print result
-            Console.WriteLine(elfAges.ContainsValue(5000));
         }
     }
 }
--- CSharpExamples/Chapter03/Loops.cs
+++ CSharpExamples/Chapter03/Loops.cs
@@ -1,6 +1,3 @@
-using System.Reflection.PortableExecutable;
-using System.Text;
-
 namespace Chapter03
 {
     public class Loops
@@ -12,29 +9,23 @@ namespace Chapter03
             /* FOR LOOP */
 
             string[] battles =
-            {
+            [
                 "The Battle of Moria",
                 "The Battle for Isengard",
                 "The Battle of Helm's Deep",
                 "The Battle of the Pelennor Fields",
                 "The Battle of the Black Gate"
-            };
+            ];
 
             // TODO: Construct a basic for loop to print each battle
-            for (int i = 0; i < battles.Length; i++)
-            {
-                Console.WriteLine(battles[i]);
-            }
+
 
             /* FOREACH LOOP */
 
             string[] words = ["YOU", "SHALL", "NOT", "PASS"];
 
             // TODO: Construct a foreach loop to print each element of the array above
-            foreach (string word in words)
-            {
-                Console.WriteLine(word);
-            }
+
 
             /* FOREACH LOOP WITH DICTIONARY */
 
@@ -50,40 +41,23 @@ namespace Chapter03
             // TODO: Construct three for-each loops for the dictionary above
 
             // Loop through keys and print each
-            foreach (string key in moria.Keys)
-            {
-                Console.WriteLine(key);
-            }
 
             // Loop through values and print each
-            foreach (string val in moria.Values)
-            {
-                Console.WriteLine(val);
-            }
 
             // Loop through key/value pairs and print them
-            foreach (KeyValuePair<string, string> kvp in moria)
-            {
-                Console.WriteLine("The " + kvp.Key + " of Moria is " + kvp.Value);
-            }
+
 
             /* FOREACH LOOP WITH STRINGBUILDER */
 
             // TODO: Use the StringBuilder class for the accumulator pattern with strings
 
-            string[] frodoLetters = ["f", "r", "o", "d", "o"];
-
             // Declare new Stringbuilder object
-            StringBuilder frodo = new StringBuilder();
 
             // Loop over and add to it a letter at a time - make it uppercase
-            foreach (string letter in frodoLetters)
-            {
-                frodo.Append(letter.ToUpper());
-            }
+
 
             // Print the final result
-            Console.WriteLine(frodo);
+
 
             /* WHILE LOOP */
 
@@ -91,39 +65,14 @@ namespace Chapter03
 
             // TODO: Construct a while loop to print each number in the Array above as long as the value is below 100
 
-            int index = 0;
-
-            while (orcs[index] < 100)
-            {
-                Console.WriteLine(orcs[index]);
-                index++;
-            }
 
             /* DO-WHILE LOOP */
 
             List<string> bookTitles = [];
 
-            string title;
-            bool isQuitting = false;
-
             // TODO: Construct a do-while loop to ask the user for book titles until they enter "QUIT"
             // Unless quitting, add each title to the collection above
-            do
-            {
-                Console.WriteLine("Please enter a book title, or hit ENTER to quit: ");
-                title = Console.ReadLine();
-                isQuitting = Equals(title.Trim(), "");
-                if (!isQuitting)
-                {
-                    bookTitles.Add(title);
-                }
-            } while (!isQuitting);
 
-            // Print all book titles in the list
-            foreach (string book in bookTitles)
-            {
-                Console.WriteLine(book);
-            }
 
             /* BREAK AND CONTINUE KEYWORDS WITH LOOPS */
 
@@ -154,34 +103,8 @@ namespace Chapter03
             // If a character is not a member, continue without checking count
             // Break the loop when all 9 members have been found
             // If a character was found but fellowship is still not at 9, print a statement
-            List<string> fellowship = [];
-
-            foreach (KeyValuePair<string, bool> lotrChar in lotrCharacters)
-            {
-                if (lotrChar.Value)
-                {
-                    fellowship.Add(lotrChar.Key);
-                    Console.WriteLine(lotrChar.Key + " added to the fellowship.");
-                }
-                else
-                {
-                    Console.WriteLine(
-                        "(" + lotrChar.Key + " is not a member of the fellowship.)\n"
-                    );
-                    continue; // will skip remaining code of loop body but continue looping
-                }
-
-                if (fellowship.Count == 9)
-                {
-                    Console.WriteLine("All members have been found!");
-                    break; // stops loop altogether
-                }
-                Console.WriteLine("Still searching for more fellowship members...\n");
-            }
 
             // Print the list of fellowship members
-            Console.WriteLine("\nThe Fellowship of the Ring:");
-            Console.WriteLine(string.Join(", ", fellowship));
         }
     }
 }
