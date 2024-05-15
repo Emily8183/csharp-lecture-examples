ddea44563930de00b9388a98cf9c603f1cd01dd1
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-15 13:08:01 -0400

    updated comments for ch 4

--- CSharpExamples/Chapter04/Program.cs
+++ CSharpExamples/Chapter04/Program.cs
@@ -2,23 +2,27 @@
 
 using Chapter04;
 
+// Create an object of the Library class
 Library library = new Library();
 
+// Create three objects of the Book class
 Book book1 = new Book("Contact", "Carl Sagan", 430);
 Book book2 = new Book("Nemesis", "Isaac Asimov", 364);
 Book book3 = new Book("1984", "George Orwell");
 
+// Add books to library
 library.AddBook(book1);
 library.AddBook(book2);
 library.AddBook(book3);
 
+// Print available books
 library.PrintAvailableBooks();
 
+// Check out book 1 and print available books again
 book1.CheckOut();
-
 library.PrintAvailableBooks();
 
+// Check out book 2, check in book 1, and print available books again
 book2.CheckOut();
 book1.CheckIn();
-
 library.PrintAvailableBooks();
