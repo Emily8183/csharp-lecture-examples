dd203998d8c8e0706bb2c7e10dd4fa9a19944d50
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-29 23:32:58 -0400

    added basic examples for ch 11

---
+++ CSharpExamples/Chapter11/NoVowelException.cs
@@ -0,0 +1,7 @@
+﻿namespace Chapter11;
+
+public class NoVowelException : Exception
+{
+    public NoVowelException(string word)
+        : base("WARNING: The word '" + word + "' has no standard vowels.") { }
+}
--- CSharpExamples/Chapter11/Program.cs
+++ CSharpExamples/Chapter11/Program.cs
@@ -1,3 +1,77 @@
-﻿/**** CHAPTER 11 EXAMPLES ****/
+﻿using Chapter11;
 
+/**** CHAPTER 11 EXAMPLES ****/
 
+string[] numbersAsText = ["zero", "one", "two", "three", "four"];
+PrintElementChars(numbersAsText, 7);
+
+Console.WriteLine("The square root of 16 is " + GetSquareRoot(16));
+Console.WriteLine("The square root of -5 is " + GetSquareRoot(-5));
+
+String[] storyTypes = ["epic", "fable", "myth", "folktale"];
+
+foreach (string type in storyTypes)
+{
+    try
+    {
+        if (!HasAVowel(type))
+        {
+            throw new NoVowelException(type);
+        }
+        else
+        {
+            Console.WriteLine(type);
+        }
+    }
+    catch (NoVowelException e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+static void PrintElementChars(string[] strArray, int idx)
+{
+    try
+    {
+        for (int i = 0; i < strArray[idx].Length; i++)
+        {
+            Console.WriteLine(strArray[idx][i]);
+        }
+    }
+    catch (IndexOutOfRangeException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+static double GetSquareRoot(int n)
+{
+    try
+    {
+        if (n < 0)
+        {
+            throw new ArgumentOutOfRangeException(
+                "WARNING: Cannot take square root of negative number " + n
+            );
+        }
+    }
+    catch (ArgumentOutOfRangeException e)
+    {
+        Console.WriteLine(e);
+    }
+    return Math.Sqrt(Math.Abs(n));
+}
+
+static bool HasAVowel(string word)
+{
+    bool hasVowel = false;
+    foreach (char letter in word.ToCharArray())
+    {
+        if ("aeiou".Contains(letter))
+        {
+            hasVowel = true;
+            break;
+        }
+    }
+    return hasVowel;
+}
