1f6bfcb40cd491026dbb433fee216a9f7be0bc6f
Emily Lin <116020783+Emily8183@users.noreply.github.com>
2024-10-16 17:28:28 -0500

    Update Constants.cs
    update the concept of const keyword

--- CSharpExamples/Chapter05/Constants.cs
+++ CSharpExamples/Chapter05/Constants.cs
@@ -3,5 +3,12 @@ namespace Chapter05
     public class Constants
     {
         public const string DASHED_LINE = "------------------------------------------------";
+        //const has the same value across all objects of a class
+        //cannot be set at runtime, only compile time
+        //must be public
+        //set in SCREAMING_SNAKE_CASE
+        //no need to add static, its behind the scenes
+        //the value is stored at the class level and can't be changed    
+        
     }
 }
